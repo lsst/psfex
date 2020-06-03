d7c548f1dab761e02f0f0049729d2fc2f582745b
Kian-Tat Lim <ktl@slac.stanford.edu>
2020-06-02 19:23:23 -0500

    Build under conda with minimal changes.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -7,15 +7,6 @@ config()
  	trap finish EXIT
 	options="--prefix=$PREFIX --disable-threads --with-lapack-stub "
 	options=$options"--enable-plplot=no "
-	# The following if statement allows the package to be built if
-	# someone chooses to use their system libraries for fftw in place of
-	# the package provided through eups. It checks if the eups fftw
-	# environment variable is set, and if it is the configuration is
-	# set appropriately.
-	if [ "$FFTW_DIR" ]; then
-		options=$options"--with-fftw-incdir=$FFTW_DIR/include "
-		options=$options"--with-fftw-libdir=$FFTW_DIR/lib "
-	fi
 	./configure ${options}
 }
 
